e3945dbf14e0f297932677de4e074e9907a7d537
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-10-25 11:08:30 +0100

    Added the article routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const connectDB = require('./DB/database')
+const connectDB = require('./DB/database');
+const articleRoutes = require('./routes/articleRoutes');
+require('dotenv').config()
 
-const dotenv = require('dotenv')
-dotenv.config()
-
-connectDB()
+connectDB()		//MongoDB database
 
 const app = express()
+app.use(express.json())	//middleware instead of bodypalser
+
+// routes
+app.use('/articles', articleRoutes);
+app.get('/home', (req, res)=>{
+	res.send('welcome');
+})
+
 
 // For the server
 const PORT = process.env.PORT || 3000
